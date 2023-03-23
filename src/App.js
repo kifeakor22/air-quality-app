6ad149d151a507c55a1dca9bd6c5d6a09cb90563
Jon Garnett-Smith <jon@exce-ed.com>
2023-03-23 21:52:58 +0000

    Completed Search component with some styling. JGS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import React from "react";
+import Search from "./components/search/Search";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Search />
+    </div>
+  );
 }
 
 export default App;

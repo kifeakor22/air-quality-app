9a7592c2de45bea3cc1280dbb52bf98889f6e826
Babe <115712127+Saadu10002@users.noreply.github.com>
2023-03-24 22:50:24 +0000

    added health and author files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import CurrentLocation from "./components/current-location/CurrentLocation";
 import Jumbotron from "./components/jumbotron";
 import Health from "./components/Health/health";
+import Author from "./components/Author/author";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Jumbotron />
       <CurrentLocation />
       <Health/>
+      <Author/>
     </div>
   );
 }
---
+++ src/components/Author/author.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './author.css'
+
+const Author = () => {
+    return <div>
+     <div className='author'>
+        <h3 className='author'>Made by</h3>
+        <p>
+            Jon<br></br>
+            Kossiochukwu<br></br>
+            joe<br></br>
+            Laura<br></br>
+            Fatima
+        </p>
+        </div>; 
+    </div>;
+        
+
+    
+    
+
+
+
+}
+
+export default Author;

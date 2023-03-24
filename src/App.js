20b08fdccf06b717d930813cee8b08c61ddee856
Babe <115712127+Saadu10002@users.noreply.github.com>
2023-03-24 17:57:36 +0000

    update the health infomation in the health file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import CurrentLocation from "./components/current-location/CurrentLocation";
 import Jumbotron from "./components/jumbotron";
+import Health from "./components/Health/health";
 
 function App() {
   return (
     <div className="App">
       <Jumbotron />
       <CurrentLocation />
+      <Health/>
     </div>
   );
 }

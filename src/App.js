76d8d609afbc8b0da1c20f87b7141d73e5436dcd
Jon Garnett-Smith <jon@exce-ed.com>
2023-03-24 08:57:42 +0000

    Initial code for building current location. JGS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import CurrentLocation from "./components/current-location/CurrentLocation";
 import Jumbotron from "./components/jumbotron";
 
 function App() {
   return (
     <div className="App">
       <Jumbotron />
-     
+      <CurrentLocation />
     </div>
   );
 }

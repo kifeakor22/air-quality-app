51895abb3df76b3a1122fad0eb48d8ef943c98b9
Al Turing <47292529+TweetingCynical@users.noreply.github.com>
2023-03-25 09:39:18 +0000

    Added conditional styling to forecast cards

--- src/components/forecast/Forecast.js
+++ src/components/forecast/Forecast.js
@@ -8,17 +8,35 @@ const Forecast = ({ forecast }) => {
 
   return (
     <div className="forecast-container">
-      {forecast.daily?.o3?.map((day) => (
-        <div className="day-card" key={day.day}>
-            <h1>{day.day}</h1>
+      {forecast.daily?.o3?.map((day) => {
+        const date = new Date(day.day);
+        const weekday = date.toLocaleString('en-US', { weekday: 'long' });
+        const avg = day.avg;
+        let bgColor;
+
+        if (avg <= 20) {
+          bgColor = 'var(--vgoodColor)';
+        } else if (avg <= 22) {
+          bgColor = 'var(--goodColor)';
+        } else if (avg <= 24) {
+          bgColor = 'var(--fairColor)';
+        } else if (avg <= 26) {
+          bgColor = 'var(--poorColor)';
+        } else {
+          bgColor = 'var(--vpoorColor)';
+        }
+
+        return (
+          <div className="day-card" key={day.day} style={{ backgroundColor: bgColor}}>
+            <h1>{weekday}</h1>
             <p>Average: {day.avg}</p>
             <p>Min: {day.min}</p>
             <p>Max: {day.max}</p>
-        </div>
-      ))}
+          </div>
+        );
+      })}
     </div>
   );
 };
 
 export default Forecast;
-

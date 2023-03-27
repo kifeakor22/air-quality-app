0c07285ffc83d01146a571cb5754c75a7aa94db5
Kc <kifeakor@yahoo.com>
2023-03-27 12:04:57 +0100

    added icons with color codes for individual pollutant in a particular city

--- src/components/airqualitystats/airqaulitystats.js
+++ src/components/airqualitystats/airqaulitystats.js
@@ -1,32 +1,48 @@
 import React from 'react'
 import './style.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 const Airquality = ({aqi,dominentpol, iaqi })=> {
+    let bgColor;
+
+    if(aqi <= 50) {
+        bgColor = 'var(--vgoodColor)';
+    } else if (aqi >= 51) {
+        bgColor = 'var(--fairColor)';
+    } else if (aqi>=101 ) {
+        bgColor = 'var(--poorColor)';
+
+    }else if (aqi >=2001 ){
+        bgColor = 'var(--vpoorColor)';
+    }
     
 return (
     <>
-   
-  <div style={{ display: "flex", justifyContent: "center" }}>
-    <div className="card airstat">
-      <div>AQI: {aqi}</div>
-      <div>Main Pollutant: {dominentpol}</div>
-      {iaqi && (
-        <div>
-          {Object.keys(iaqi).map((key) => (
-            <div className="card" style={{ width: "18rem" }}>
-              <ul className="list-group list-group-flush" style={{ listStyleType: "none" }}>
-                <li key={key}>
-                  {key}: {iaqi[key].v}
-                </li>
-              </ul>
-            </div>
-        
-          ))}
-        </div>
-      )}
+     <div style={{ display: "flex", justifyContent: "center" }}>
+      <div className="card airstat">
+        <div>AQI: {aqi}</div>
+        <div>Main Pollutant: {dominentpol}</div>
+        {iaqi && (
+          <div>
+            {Object.keys(iaqi).map((key) => (
+              <div className="card" style={{ width: "18rem" }}>
+                <ul className="list-group list-group-flush" style={{ listStyleType: "none", display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 0 }}>
+                  <li key={key}>
+                    {key}: {iaqi[key].v}
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faCircle} className='fa-beat' style={{color: bgColor, border: '1px solid white', borderRadius: '50%'}}/>
+                  </li>
+                </ul>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-   <img src={require('./images/aqiKey.PNG')} alt='color code'/>
+   
+   <img className='image-fluid' src={require('./images/aqiKey.PNG')} alt='color code'/>
   </>
 );
 

dfade98b49c5d9324e1575c8a582e1e97e31edd7
Kc <kifeakor@yahoo.com>
2023-03-27 12:15:00 +0100

    each individual pollutant is color coded based on its level in the area

--- src/components/airqualitystats/airqaulitystats.js
+++ src/components/airqualitystats/airqaulitystats.js
@@ -6,17 +6,7 @@ import { faCircle } from '@fortawesome/free-solid-svg-icons';
 const Airquality = ({aqi,dominentpol, iaqi })=> {
     let bgColor;
 
-    if(aqi <= 50) {
-        bgColor = 'var(--vgoodColor)';
-    } else if (aqi >= 51) {
-        bgColor = 'var(--fairColor)';
-    } else if (aqi>=101 ) {
-        bgColor = 'var(--poorColor)';
-
-    }else if (aqi >=2001 ){
-        bgColor = 'var(--vpoorColor)';
-    }
-    
+   
 return (
     <>
      <div style={{ display: "flex", justifyContent: "center" }}>
@@ -25,7 +15,17 @@ return (
         <div>Main Pollutant: {dominentpol}</div>
         {iaqi && (
           <div>
-            {Object.keys(iaqi).map((key) => (
+            {Object.keys(iaqi).map((key) => {
+                 if(iaqi[key].v <= 50) {
+                    bgColor = 'var(--vgoodColor)';
+            } else if (iaqi[key].v >= 51) {
+                bgColor = 'var(--fairColor)';
+            } else if (iaqi[key].v>=101 ) {
+                bgColor = 'var(--poorColor)';
+            }else if (iaqi[key].v >=2001 ){
+                bgColor = 'var(--vpoorColor)';
+            }
+            return (
               <div className="card" style={{ width: "18rem" }}>
                 <ul className="list-group list-group-flush" style={{ listStyleType: "none", display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 0 }}>
                   <li key={key}>
@@ -35,8 +35,8 @@ return (
                     <FontAwesomeIcon icon={faCircle} className='fa-beat' style={{color: bgColor, border: '1px solid white', borderRadius: '50%'}}/>
                   </li>
                 </ul>
-              </div>
-            ))}
+              </div>)
+})}
           </div>
         )}
       </div>

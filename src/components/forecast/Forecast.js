13adcc7bcfc6ca66d5002c3918e29e185d735dcb
Jon Garnett-Smith <jon@exce-ed.com>
2023-03-27 14:28:06 +0100

    Added IF to forecast cards to remove old days. JGS

--- src/components/forecast/Forecast.js
+++ src/components/forecast/Forecast.js
@@ -1,33 +1,45 @@
-import React from 'react';
+import React from "react";
 import "./style.css";
 
-const Forecast = ({ forecast, dominentpol}) => {
+const Forecast = ({ forecast, dominentpol }) => {
   if (!forecast) {
-    return <div>No data available</div>
+    return <div>No data available</div>;
   }
+
+  const today = new Date();
+  today.setHours(0, 0, 0, 0); // Sets so that it is not actual time, but start of the day
 
   return (
     <div className="forecast-container">
       {forecast.daily?.[dominentpol]?.map((day) => {
         const date = new Date(day.day);
-        const weekday = date.toLocaleString('en-US', { weekday: 'long' });
+        // Return null for days before today to remove old dates from data
+        if (date < today) {
+          return null;
+        }
+
+        const weekday = date.toLocaleString("en-US", { weekday: "long" });
         const avg = day.avg;
         let bgColor;
 
-        if (avg <= 20) {
-          bgColor = 'var(--vgoodColor)';
-        } else if (avg <= 22) {
-          bgColor = 'var(--goodColor)';
-        } else if (avg <= 24) {
-          bgColor = 'var(--fairColor)';
-        } else if (avg <= 26) {
-          bgColor = 'var(--poorColor)';
+        if (avg <= 50) {
+          bgColor = "var(--vgoodColor)";
+        } else if (avg <= 100) {
+          bgColor = "var(--goodColor)";
+        } else if (avg <= 150) {
+          bgColor = "var(--fairColor)";
+        } else if (avg <= 200) {
+          bgColor = "var(--poorColor)";
         } else {
-          bgColor = 'var(--vpoorColor)';
+          bgColor = "var(--vpoorColor)";
         }
 
         return (
-          <div className="day-card" key={day.day} style={{ backgroundColor: bgColor}}>
+          <div
+            className="day-card"
+            key={day.day}
+            style={{ backgroundColor: bgColor }}
+          >
             <h1>{weekday}</h1>
             <p>Average: {day.avg}</p>
             <p>Min: {day.min}</p>

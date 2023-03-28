d56e494212b342975c3412d1db985ac12b8a1090
Kc <kifeakor@yahoo.com>
2023-03-28 12:24:10 +0100

    added description for each pollutants

--- src/components/airqualitystats/airqaulitystats.js
+++ src/components/airqualitystats/airqaulitystats.js
@@ -2,14 +2,28 @@ import React from 'react'
 import './style.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import pollutantsData from './pollutants.json'
 
 const Airquality = ({aqi,dominentpol, iaqi })=> {
     let bgColor;
+    const pollutants = pollutantsData[0];
+
 
    
 return (
-    <>
+    <>  
      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div>
+            <h3>Pollutants description</h3>
+      {Object.entries(pollutants).map(([key, value]) => (
+        <div className='card airstat' style={{width: "18rem"}} key={key}>
+          <ul className='list-group list-group-flush' style={{ listStyleType: "none"}}>
+            <li>{key}: {value}</li>
+          </ul>
+        </div>
+      ))}
+    
+    </div>
       <div className="card airstat">
         <div>AQI: {aqi}</div>
         <div>Main Pollutant: {dominentpol}</div>
@@ -23,17 +37,17 @@ return (
                 bgColor = 'var(--fairColor)';
             } else if (iaqi[key].v>=101 ) {
                 bgColor = 'var(--poorColor)';
-            }else if (iaqi[key].v >=2001 ){
+            }else if (iaqi[key].v >=201 ){
                 bgColor = 'var(--vpoorColor)';
             }
             return (
               <div className="card" style={{ width: "18rem" }}>
                 <ul className="list-group list-group-flush" style={{ listStyleType: "none", display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 0 }}>
                   <li key={key}>
-                    {key}: {iaqi[key].v}
+                    {key}: {Math.floor(iaqi[key].v)}
                   </li>
                   <li>
-                    <FontAwesomeIcon icon={faCircle} className='fa-beat' style={{color: bgColor, border: '1px solid white', borderRadius: '50%'}}/>
+                    <FontAwesomeIcon icon={faCircle} className='fa-beat' style={{color: bgColor, border: '1px solid white', borderRadius: '50%'}}   title={`${key}: ${Math.floor(iaqi[key].v)}`}/>
                   </li>
                 </ul>
               </div>)
@@ -42,8 +56,7 @@ return (
         )}
       </div>
     </div>
-   
-   <img className='image-fluid' src={require('./images/aqiKey.PNG')} alt='color code'/>
+   <img className='image-fluid code' src={require('./images/aqiKey.PNG')} alt='color code'/>
   </>
 );
 

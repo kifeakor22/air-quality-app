38b08671c8e72e9cfc4671a087dc591a80c22713
Babe <115712127+Saadu10002@users.noreply.github.com>
2023-03-29 19:20:08 +0100

    update the files

--- src/components/Health/health.js
+++ src/components/Health/health.js
@@ -1,31 +1,33 @@
-import React from 'react';
-import './health.css'
-const Health = () => {
-    return(
-        <div>
-            <div className='info-container'>
-            <div className="information">
-              <h2 className='health-info'>Health information</h2>
-              <p>
-                Measures concentrations of pollutants on a 1-500 scale. You may need to change your outdoor exercise schedule or location depending on pollutants concentrations:<br></br>
-                Good(0-5): Air quality is good. it's safe to exercise outdoors.<br></br>
-                Moderate(26-50): Air quality is fine.<br></br>
-                Raised level of pollutants may affect your breathing if you exercise outdoors especially if you have asthma or allergies.<br></br>
-                Moderately unhealth(51-100): Raised levels of pollutants amy affect your breathing if you exercise outdoors,even for healthy individuals. Pollutants can cause health problems if you have a lung or heart condition. try to exercise indoors or keep your outdoors workout short.<br></br>
-                unhealth for sensitive groups(101-150): pollutants concentrations affect everyone, especially those with lung or heart problems. Exercise indoors to reduce exposure to pollution.<br></br>
-                Veryunhealthy(151-200): Pollutants concentrations can cause health problems for everyone. DO NOT exercise outdoors.<br></br>
-                Hazardous(201-500): Air quality is bad enough to be declared an emergency. stay indoors or evacuate the area until air quality improves.
+import React from "react";
+import "./style.css";
+import healthData from "./health.json";
 
+const Health = ({ dominentpol }) => {
+  const filteredData = healthData.filter((item) => item.id === dominentpol);
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div>
+        <h3>Pollutants description</h3>
+        {filteredData.map((item) => (
+          <div
+            className="card airstat"
+            style={{ width: "18rem" }}
+            key={item.id}
+          >
+            <ul
+              className="list-group list-group-flush"
+              style={{ listStyleType: "none" }}
+            >
+              <li>
+                <span className="bold">{item.name}:</span> {item.about}
+                <div className="risk">{item["health risk"]}</div>
+              </li>
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-              </p>
-
-
-            </div>
-            </div>
-        </div>
-
-    )
-
-
-}
 export default Health;

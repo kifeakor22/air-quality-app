92c7ca9567bf534cd7b8f8484d5107a8b54270ff
lauren-mac <109279133+lauren-mac@users.noreply.github.com>
2023-03-30 20:27:42 +0100

    fix to text

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -9,16 +9,10 @@ const Jumbotron = () => {
           <div className="image"></div>
           <div className="jumbotron">
             <p className="lead">
-              Welcome to our user-friendly air quality monitoring app! Enter
-              your location and get real-time updates on air quality.
+              Welcome to our Air Quality Monitoring app! Enter the name of a city into the search bar above and get real-time updates on air quality.
             </p>
             <p className="lead">
-              Our app empowers you with knowledge to make informed decisions
-              about your health. 
-            </p>
-            <p className="lead">
-              <b>Enter the name of a city into the search bar above to get real-time air quality updates to protect
-              yourself and loved ones from harmful pollutants.</b>
+              Our app empowers you with knowledge to make informed decisions to protect yourself and loved ones from harmful pollutants.
             </p>
             <hr className="my-4" />
           </div>
